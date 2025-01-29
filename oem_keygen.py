2ac97002be13fd4e733caac3ad34973f79a82c67
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-01-29 08:52:50 +0100

    Create oem_keygen.py

---
+++ oem_keygen.py
@@ -0,0 +1,22 @@
+import sys
+from hashlib import sha256
+
+def oem_keygen(key: str) -> str:
+    to_hash: str = key * 2
+    print("To hash: ", to_hash)
+
+    hash: str = sha256(to_hash.encode()).hexdigest()
+
+    print("Hash: ", hash)
+    print("Possible keys:\n%s\n%s" % (hash[:32], hash[32:]))
+    print("Capitalized:\n%s\n%s" % (hash[:32].upper(), hash[32:].upper()))
+    return hash
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python3 oem_keygen.py <key>")
+        print("First run `fastboot oem get_key` to get the key")
+        sys.exit(0)
+
+    oem_keygen(sys.argv[1])
