db8badc3cf946c7beaf0063112d4c5454c7611e1
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-03-09 19:31:00 +0100

    Add lamu definitions

--- utils/constants.py
+++ utils/constants.py
@@ -14,7 +14,16 @@ DEVICES: dict = {
         "preloader": "preloader_p235a.bin",
         "scatter": "MT6768_Android_scatter.txt",
         "partition_scheme": "fogorow_partitions.csv",
-        "has_unlock_method": False,
+        "has_unlock_method": False, # Have to get engineering DA first, but can be unlocked: https://fogorow.fuckyoumoto.xyz/dev/bootloader
         "preferred_da": 0,
     },
+    "lamu":
+    {
+        "da_files": ["DA_A15_lamu.bin", "DA_SWSEC_2404_lamu_dl_forbidden.bin"],
+        "preloader": "preloader_lamu.bin",
+        "scatter": "MT6768_Android_scatter.txt",
+        "partition_scheme": "", # To add
+        "has_unlock_method": False, # Problably can be unlocked the same way G24 can
+        "preferred_da": 0,
+    }
 }
